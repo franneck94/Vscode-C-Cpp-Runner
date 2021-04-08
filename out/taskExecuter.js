3f7b678cc24af5e8d8183839217224828b74d000
franneck94 <schaffranek.jan@web.de>
2021-04-08 13:50:44 +0200

    Remove ignored files

--- out/taskExecuter.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// class TasksExecutor implements vscode.Pseudoterminal {
-// 	onDidWrite() => void {
-// 	}
-// }
-//# sourceMappingURL=taskExecuter.js.map

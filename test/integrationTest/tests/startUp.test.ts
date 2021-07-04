00a8ab233725c840fa3e30d8618b1de3fac8ae57
franneck94 <schaffranek.jan@web.de>
2021-07-04 11:13:50 +0200

    Updated integration test

--- test/integrationTest/tests/startUp.test.ts
+++ test/integrationTest/tests/startUp.test.ts
@@ -5,28 +5,39 @@ import { activateCppExtension } from '../testHelper';
 
 suite('startup tests', function () {
   suiteSetup(async function () {
+    const file_content = `
+    #include <stdio.h>
+
+    int main()
+    {
+      printf("Hello World!");
+
+      return 0;
+    }
+    `;
+
     let extension = activateCppExtension();
-    // let document = await vscode.workspace.openTextDocument({
-    //   language: 'cpp',
-    //   content: 'int main() { return 0; }',
-    // });
-    // let textEitor = await vscode.window.showTextDocument(document);
+    let document = await vscode.workspace.openTextDocument({
+      language: 'cpp',
+      content: file_content,
+    });
+    let textEitor = await vscode.window.showTextDocument(document);
 
     assert.notStrictEqual(extension, undefined);
-    // assert.notStrictEqual(document, undefined);
-    // assert.notStrictEqual(textEitor, undefined);
+    assert.notStrictEqual(document, undefined);
+    assert.notStrictEqual(textEitor, undefined);
   });
 
   suiteTeardown(async function () {});
 
   test('Check default configuration', () => {
-    // let rootUri: vscode.Uri | undefined;
-    // if (
-    //   vscode.workspace.workspaceFolders &&
-    //   vscode.workspace.workspaceFolders.length > 0
-    // ) {
-    //   rootUri = vscode.workspace.workspaceFolders[0].uri;
-    // }
-    // assert.notStrictEqual(rootUri, undefined, 'Root Uri is not defined');
+    let rootUri: vscode.Uri | undefined;
+    if (
+      vscode.workspace.workspaceFolders &&
+      vscode.workspace.workspaceFolders.length > 0
+    ) {
+      rootUri = vscode.workspace.workspaceFolders[0].uri;
+    }
+    assert.notStrictEqual(rootUri, undefined, 'Root Uri is not defined');
   });
 });

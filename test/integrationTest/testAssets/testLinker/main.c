4adf8cb3d8a01a167e08d9d7bdb21be91fd62924
franneck94 <schaffranek.jan@web.de>
2021-08-15 16:21:40 +0200

    Added linker example

---
+++ test/integrationTest/testAssets/testLinker/main.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+void *print_message_function(void *ptr)
+{
+    char *message;
+    message = (char *)ptr;
+    printf("%s \n", message);
+}
+
+int main()
+{
+    pthread_t thread1, thread2;
+    char *message1 = "Thread 1";
+    char *message2 = "Thread 2";
+    int  iret1, iret2;
+
+    iret1 = pthread_create(&thread1, NULL, print_message_function, (void *)message1);
+    iret2 = pthread_create(&thread2, NULL, print_message_function, (void *)message2);
+
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+
+    printf("Thread 1 returns: %d\n", iret1);
+    printf("Thread 2 returns: %d\n", iret2);
+
+    return 0;
+}

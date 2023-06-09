f600e082ada3def4f3f2c042d5d42959a74af79e
franneck94 <schaffranek.jan@web.de>
2023-06-09 20:25:33 +0200

    updated example

--- test/testNested/main.c
+++ test/testNested/main.c
@@ -3,9 +3,15 @@
 int main()
 {
 #ifndef NDEBUG
-    printf("Debug Mode");
+    printf("Debug Mode 1\n");
 #else
-    printf("Release Mode");
+    printf("Release Mode 1\n");
+#endif
+
+#ifdef _DEBUG
+    printf("Debug Mode 2\n");
+#else
+    printf("Release Mode 2\n");
 #endif
 
     return 0;

6040c31c0e480dab82f68513a447be8d535cdb75
franneck94 <schaffranek.jan@web.de>
2023-06-27 08:55:50 +0200

    moved include paths funcs to here

---
+++ src/utils/compilerUtils.ts
@@ -0,0 +1,39 @@
+export function gatherIncludeDirsUnix(includePaths: string[]) {
+  let args = '';
+
+  if (includePaths && includePaths.length > 0) {
+    for (const includePath of includePaths) {
+      if (includePath.includes('$(default)')) continue;
+
+      const hasSpace = includePath.includes(' ');
+
+      if (hasSpace) {
+        args += ` -I"${includePath}"`;
+      } else {
+        args += ` -I${includePath}`;
+      }
+    }
+  }
+
+  return args;
+}
+
+export function gatherIncludeDirsMsvc(includePaths: string[]) {
+  let args = '';
+
+  if (includePaths && includePaths.length > 0) {
+    for (const includePath of includePaths) {
+      if (includePath.includes('$(default)')) continue;
+
+      const hasSpace = includePath.includes(' ');
+
+      if (hasSpace) {
+        args += ` /I"${includePath}"`;
+      } else {
+        args += ` /I${includePath}`;
+      }
+    }
+  }
+
+  return args;
+}

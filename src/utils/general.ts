9d8a584a0dca8c98f19b43bdf9af8151232a4c72
franneck94 <schaffranek.jan@web.de>
2023-08-29 13:31:43 +0200

    unused

--- src/utils/general.ts
+++
@@ -1,9 +0,0 @@
-export function arraysEqual(a: any[], b: any[]) {
-  if (a === b) return true;
-  if (a.length !== b.length) return false;
-
-  for (let i = 0; i < a.length; ++i) {
-    if (a[i] !== b[i]) return false;
-  }
-  return true;
-}
